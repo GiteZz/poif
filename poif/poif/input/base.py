5fcb34e42fba8d6d5736e6f72b4fa6ae7e238e7a
gillesballegeer <gilles.ballegeer@ugent.be>
2021-01-19 09:54:23 +0100

    Input idea

--- poif/poif/input/base.py
+++ poif/poif/input/base.py
@@ -1,7 +1,34 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+
+from poif.tagged_data.base import TaggedData
+from typing import List
 
 
-class Input(ABC):
+@dataclass
+class DataSetObject:
+    data: TaggedData = None
+    objects: List['DataSetObject'] = field(default_factory=list)
+
+
+class Image(DataSetObject):
+    width: int = None
+    height: int = None
+
+
+class Point(DataSetObject):
+    x: float = None
+    y: float = None
+
+
+class Rectangle(DataSetObject):
+    x: float
+    y: float
+    w: float
+    h: float
+
+
+class Input(DataSetObject, ABC):
     @abstractmethod
     def output(self):
         pass
